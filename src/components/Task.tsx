a31383f11299fb3d685ce8f388c0ce52c843fb13
solidados <geekbrains.pk@internet.ru>
2024-09-07 00:29:54 +0400

    feat: Task: create component

---
+++ src/components/Task.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react'
+
+interface TaskProps {
+  task: {
+    id: string,
+    title: string,
+    state: string
+  },
+  onArchiveTask: (id: string) => void,
+  onPinTask: (id: string) => void
+}
+
+const Task: FC<TaskProps> = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
+  return (
+    <div className="list-item">
+      <label htmlFor={`title-${id}`} area-label={title}>
+        <input type="text" value={title} readOnly={true} name={title} id={`title-${id}`} />
+      </label>
+      <button onClick={() => onArchiveTask(id)}>Archive</button>
+      <button onClick={() => onPinTask(id)}>Pin</button>
+    </div>
+  );
+}
+
+export default Task;
