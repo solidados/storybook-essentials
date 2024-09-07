0d960353754344876a04862619968581a3e812df
solidados <geekbrains.pk@internet.ru>
2024-09-07 20:10:01 +0400

    refactor: update Task component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -12,12 +12,46 @@ interface TaskProps {
 
 const Task: FC<TaskProps> = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
   return (
-    <div className="list-item">
-      <label htmlFor={`title-${id}`} area-label={title}>
-        <input type="text" value={title} readOnly={true} name={title} id={`title-${id}`} />
+    <div className={`list-item ${state}`}>
+      <label
+        htmlFor={`archiveTask-${id}`}
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
+        />
+        <span
+          className="checkbox-custom"
+          onClick={(): void => onArchiveTask(id)}
+        />
       </label>
-      <button onClick={(): void => onArchiveTask(id)}>Archive</button>
-      <button onClick={(): void => onPinTask(id)}>Pin</button>
+
+      <label htmlFor={`title-${id}`} aria-label={title} className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          id={`title-${id}`}
+          placeholder="Input title"
+        />
+      </label>
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={(): void => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   );
 }
