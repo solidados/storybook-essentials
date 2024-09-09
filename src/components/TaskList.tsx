07e43f5088088369af303d067c0eb839a16cd7ef
solidados <geekbrains.pk@internet.ru>
2024-09-09 11:42:45 +0400

    refactor: update array of LoadingRows in TaskList component

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -20,12 +20,44 @@ const TaskList: React.FC<TaskListProps> = ({ loading, tasks, onPinTask, onArchiv
     onArchiveTask
   }
 
-  if (loading) return <div className='list-items'>Loading...</div>
-  if (tasks.length === 0) return <div className='list-items'>Empty</div>
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  )
+
+  if (loading) return (
+    <div className='list-items' data-testid="loading" key={'loading'}>
+      {Array(tasks.length || 6)
+        .fill(0)
+        .map((_, index) => (
+          <div key={index}>{LoadingRow}</div>
+        ))
+      }
+    </div>
+  )
+
+  if (tasks.length === 0) return (
+    <div className='list-items' data-testid="empty" key={'empty'}>
+      <div className="wrapper-message">
+        <span className='icon-check' />
+        <p className="title-message">You have no tasks</p>
+        <p className="subtitle-message">Sit back and relax</p>
+      </div>
+    </div>
+  )
+
+  const tasksInOrder = [
+    ...tasks.filter((task): boolean => task.state === "TASK_PINNED"),
+    ...tasks.filter((task): boolean => task.state !== "TASK_PINNED"),
+  ]
 
   return (
     <div className="list-items">
-      {tasks.map(task => (
+      {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
